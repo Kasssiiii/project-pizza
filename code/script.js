963d1d3672313acf3da527f0de67bac885b340d3
Kasssiiii <kasia.gru@gmail.com>
2024-09-09 01:02:35 +0200

    This commit is for Step 2of the project

--- code/script.js
+++ code/script.js
@@ -1,20 +1,19 @@
 // Start of the project
 
 // Step 1 - Welcome and introduction
-
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 // Asking for customer's name
-let userName = prompt('What is your name?');
+let userName = prompt('Please enter your name');
 //Greeting the customer 
 alert(`Hi, ${userName}!`);
 
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = prompt('What would you like to order today? Please enter the number from the following options: \n1. Pizza\n2. Pasta\n3. Salad')
 
-// Step 3 - Subtype choice
-// Your code goes here
+// Step 3 - Determine customer's choice
+
 
 // Step 4 - Age
 // Your code goes here
